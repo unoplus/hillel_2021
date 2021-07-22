715d7ed91eed3149d4bf689ed2235f0dbf0e9317
unoplus <unoplus6t@gmail.com>
2021-07-22 13:41:22 +0300

    python eggs :)

---
+++ Lesson_02/eggs.py
@@ -0,0 +1,11 @@
+""" (from __future__ import braces) - выдаст только интересную ошибку
+(SyntaxError: not a chance), поэтому мы закомментируем это выражение. """
+# from __future__ import braces
+
+""" (import this) - выдаст тект (Дзен по питону),
+(import antigravity) - откроет страницу в браузере (https://xkcd.com/353/),
+ (import __hello__) - выведет строку приветствия (Hello world!) """
+import this
+import antigravity
+import __hello__
+
