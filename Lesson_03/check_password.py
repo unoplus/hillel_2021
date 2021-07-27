f5d94e0f39b9c94d1bfa1f958dc413247c0ed7c4
unoplus <unoplus6t@gmail.com>
2021-07-27 23:11:40 +0300

    Check user password

---
+++ Lesson_03/check_password.py
@@ -0,0 +1,13 @@
+# Create by Bender
+"""
+Написать програму для проверки пароля.
+У пользователя запрашивается пароль сравнивается с ожидаемой фразой -
+нужно сообщить о том что пароль совпал или пароль не совпал
+"""
+
+your_password = "Password"
+user_password = input("Type your password: ")
+if user_password in your_password:
+    print("Granted!")
+else:
+    print("Denied!")
