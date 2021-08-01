d18296f74eab717b327c39b37624e8ed1b45dd72
unoplus <unoplus6t@gmail.com>
2021-08-01 23:18:58 +0300

    вывод автоморфных чисел из пользовательского числа

---
+++ Lesson_04/automorph.py
@@ -0,0 +1,23 @@
+# Create by Bender
+
+"""
+Натуральное число называется автоморфным, если оно равно последним цифрам своего квадрата.
+Например, 25**2 = 625. Напишите программу, которая получает натуральное число и выводит
+на экран все автоморфные числа, не превосходящие N.
+"""
+
+result, count, divider = 0, 1, 10
+
+while True:
+    cmd = input("Введите натуральное число: ")
+    if cmd.isdigit():
+        number = int(cmd)
+        break
+
+while result < number:
+    result = count**2
+    if count == divider:
+        divider *= 10
+    if (result * result) % divider == count:
+        print(result)
+    count += 1
