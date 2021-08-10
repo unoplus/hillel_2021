d027c7609e322fb6119eef4e20380340a97be512
unoplus <unoplus6t@gmail.com>
2021-08-11 02:58:43 +0300

    определить количество разных чисел в списке

---
+++ Lesson_07/two_lists.py
@@ -0,0 +1,18 @@
+# Create by Bender
+
+"""
+Даны два списка чисел. Посчитайте, сколько чисел содержится одновременно как в первом списке,
+так и во втором.
+- Примечание. Эту задачу на python можно решить в одну строчку.
+"""
+
+from random import randint
+
+# С заранее определёнными списками
+user_list1 = [1, 1, 2, 3, 4, 2, 5]
+user_list2 = [6, 7, 8, 5, 0]
+print(len(set.symmetric_difference(set(user_list1), set(user_list2))))
+
+# С использованием генераторов
+print(len(set.symmetric_difference(set([randint(1, 25) for i in range(10)]),\
+                                   set([randint(10, 50) for j in range(15)]))))
