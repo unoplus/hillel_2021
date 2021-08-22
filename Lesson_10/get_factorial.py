487d743ea53e8d4c428e6106a0a27d1e0fb7d22a
unoplus <unoplus6t@gmail.com>
2021-08-22 23:40:56 +0300

    Факториал чистла

---
+++ Lesson_10/get_factorial.py
@@ -0,0 +1,16 @@
+# Create by Bender
+
+def get_factorial(n: int) -> int:
+    """
+    Факториал чистла!
+    :param n: целочисленное факториал которого необходимо вычислить
+    :return: возвращает целое число результат вычисления факториала от n
+    """
+
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * get_factorial(n - 1)
+
+
+print(get_factorial(5))
