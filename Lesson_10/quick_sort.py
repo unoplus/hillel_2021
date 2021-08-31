b356673c533ee6e55dd7966cf22ba5481b3716ed
unoplus <unoplus6t@gmail.com>
2021-08-31 22:59:06 +0300

    Быстрая сортировка

---
+++ Lesson_10/quick_sort.py
@@ -0,0 +1,43 @@
+def partition(nums, low, high):
+    # Выбираем средний элемент в качестве опорного
+    # Также возможен выбор первого, последнего
+    # или произвольного элементы
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), меняем их местами
+        nums[i], nums[j] = nums[j], nums[i]
+
+
+# 0(n log n)
+def quick_sort(nums):
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # получаем индекс опорного элемента относительно которого разделим
+            # последовательность
+            split_index = partition(items,  low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums)-1)
+
+
+if __name__ == "__main__":
+    lst = [6, 36, 69, 78, 28, 19, 300, 4, 90]
+    print(lst)
+    quick_sort(lst)
+    print(lst)
