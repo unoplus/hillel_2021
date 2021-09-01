8b0d7ada67e71adea99f04450265ecd92a81c3b6
unoplus <unoplus6t@gmail.com>
2021-09-01 21:34:15 +0300

    Псевдоним

---
+++ Lesson_11/alias_module.py
@@ -0,0 +1,11 @@
+# Create by Bender
+"""
+Задание псевдонима для импортированного модуля.
+"""
+
+from math import pi, floor as f
+
+print(pi)  # Вывод числа pi
+
+# Вызываем функцию floor через f
+print(f(5.49))
