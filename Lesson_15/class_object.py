3896e00c12c7208a533f77c38f099e6af95ab4f0
unoplus <unoplus6t@gmail.com>
2021-09-10 02:12:19 +0300

    объект класса

---
+++ Lesson_15/class_object.py
@@ -0,0 +1,14 @@
+# Create by Bender
+
+class MyFirstClass:
+    pass
+
+
+# Убедимся, что класс описывает тип данных
+print(type(MyFirstClass))
+
+# Создадим объект класса
+first_object = MyFirstClass()
+# Получим информацию об объекте
+# Убеждаемся, что это объект класса MyFirstClass
+print(type(first_object))
